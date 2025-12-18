2fdc3acd57da3ab91ea1b2923f184085459a4aea
Kaushal Dinesh Rajmandai <87120610+kaushalrajmandai@users.noreply.github.com>
2025-12-18 17:21:50 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array((1,2,3,4,5))
+
+print(arr)
+
+import numpy as np
+
+arr = np.array ([1.1,2.1,3.1])
+newarr = arr.astype(int)
+
+print(newarr)
+print(newarr.dtype)
