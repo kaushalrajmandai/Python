68a33b44766861d94c5406e21ce50d370d3728d1
Kaushal Dinesh Rajmandai <87120610+kaushalrajmandai@users.noreply.github.com>
2025-12-18 17:24:40 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,11 @@
+str = "150"
+
+print(f"Original Value: '{str}'")
+print(f"Original Type: {type(str)}")
+
+
+int12 = int(str)
+
+print(f"Converted Value: {int12}")
+print(f"Converted Type: {type(int12)}")
+print(f"Test Operation (Value + 20): {int12 + 20}")
